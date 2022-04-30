4f7e58345ca1321f3c9b03d71506abbf49cb5fd9
dogustuluk <dogus.tuluk@gmail.com>
2022-04-30 16:11:04 +0300

    Add project files.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace RabbitMQ.publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://nxdranwu:n_3nr-xZlXx0NoCWuFP05gTqZfp7_hwK@sparrow.rmq.cloudamqp.com/nxdranwu ");
+
+        }
+    }
+}
