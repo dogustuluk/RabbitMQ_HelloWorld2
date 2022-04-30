21ddc42a17681b970121f471d7f8b7fd72c93f45
dogustuluk <dogus.tuluk@gmail.com>
2022-04-30 16:14:02 +0300

    publisher created

--- RabbitMQ.publisher/Program.cs
+++ RabbitMQ.publisher/Program.cs
@@ -1,5 +1,6 @@
 ﻿using RabbitMQ.Client;
 using System;
+using System.Text;
 
 namespace RabbitMQ.publisher
 {
@@ -9,6 +10,14 @@ namespace RabbitMQ.publisher
         {
             var factory = new ConnectionFactory();
             factory.Uri = new Uri("amqps://nxdranwu:n_3nr-xZlXx0NoCWuFP05gTqZfp7_hwK@sparrow.rmq.cloudamqp.com/nxdranwu ");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("hello-queue2",true,false,false);
+            var message = "hello world 2";
+            var messageBody = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(string.Empty, "hello-queue2", null, messageBody);
+            Console.WriteLine("Mesaj Gönderilmiştir");
+            Console.ReadLine();
 
         }
     }
